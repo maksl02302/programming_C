5e440d120be66da6f450efd363d47f4d4480acaf
maksl02302 <maks_l02@mail.ru>
2025-04-26 11:22:24 +1000

    Add files via upload

--- laba6/main.c
+++ laba6/main.c
@@ -1,53 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "matrix.h"
+#include <math.h>
 
-int main() {
-	char s;
-	printf("enter +, - or * \n");
-	scanf("%c", &s);
-	int n;
-	printf("enter n \n");
-	scanf("%d", &n);
-	double* A = (double*)malloc(n * n * sizeof(double));
-    double* B = (double*)malloc(n * n * sizeof(double));
-	
-	printf("enter the elements of A:\n");
-	int i;
-    for (i = 0; i < n * n; i=i+1) {
-        scanf("%lf", &A[i]);
+int main()
+{
+    FILE *fp1, *fp2;
+    
+    fp1 = fopen("input.txt", "w");
+    char s[10] = "abcdefghij";
+    char s2[10] = "0123456789";
+    
+    for(int i = 0; i<20; i++){
+        fprintf(fp1, "%c%c %c%c %c%c 19%c%c\n", s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s2[rand()%10],s2[rand()%10]);
     }
-
-    printf("enter the elements of B:\n");
-    for (i = 0; i < n * n; i=i+1) {
-        scanf("%lf", &B[i]);
-	}
-	double* itog = NULL;
-
-    if (s =='+')
-	{
-    	itog = C(A, B, n);
-	}
-	
-	else if (s=='-'){
-    	itog = D(A, B, n);
-	}
-	else if (s=='*'){
-    	itog = E(A, B, n);
-	}
-	else{
-		printf("wrong operation :(");
-	}
-	if (itog != NULL) {
-        printf("itog \n");
-        int i;
-        for (i = 0; i < n; i++) {
-        	int j;
-            for (j = 0; j < n; j++) {
-                printf("%lf", itog[i*n+j]);
-            }
-            printf(" \n");
-        }
+    
+    
+    fp1 = fopen("input.txt", "r");
+    fp2 = fopen("output.txt", "w");
+    char s1[100];
+    
+    while(fgets(s1, 50, fp1)){
+        char s2[100];
+        strncpy(s2, s1+strlen(s1)-5, strlen(s1));
+        if(atoi(s2) > 1980)
+            fprintf(fp2, "%s", s1);
     }
-	return 0;
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
 }
